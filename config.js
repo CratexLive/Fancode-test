15ce338797ec041497779c99c692e216ece5b0a3
CratexLive <drivexluxe@gmail.com>
2026-09-09 21:10:50 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const CRICX_CONFIG = {
-    // This is the URL to your Cloudflare Worker
-    API_URL: "https://football.freeshow.fun/",
-    TELEGRAM_URL: "https://t.me/+xSqMXDXp78ZiOWQ1"
-};
