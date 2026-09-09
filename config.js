6f40ac77ad92900d8ba923d007634b4e55193dba
CratexLive <drivexluxe@gmail.com>
2026-09-09 20:40:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CRICX_CONFIG = {
+    // This is the URL to your Cloudflare Worker
+    API_URL: "https://football.freeshow.fun/",
+    TELEGRAM_URL: "https://t.me/+xSqMXDXp78ZiOWQ1"
+};
